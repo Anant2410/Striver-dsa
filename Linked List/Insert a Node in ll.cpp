83afa4ce947ae830e320b263fdaf9cfcc0fb2b79
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-09-29 00:39:46 +0530

    Update Insert a Node in ll.cpp

--- Linked List/Insert a Node in ll.cpp
+++ Linked List/Insert a Node in ll.cpp
@@ -1,14 +1,32 @@
-priority_queue<int,vector<int>,greater<int>>pq;
-     Node *temp = *head;
-     while(temp != NULL)
-     {
-         pq.push(temp->data);
-         temp = temp->next;
-     }
-     temp = *head;
-     while(pq.empty()== false)
-     {
-         temp->data = pq.top();
-         pq.pop();
-         temp = temp->next;
-     }
+class Solution{
+  public:
+    //Function to insert a node at the beginning of the linked list.
+    Node *insertAtBegining(Node *head, int x) {
+       Node* temp = new Node(x);
+       if(head==NULL)
+       {
+           return temp;
+       }
+       temp->next = head;
+       head = temp;
+       return head;
+    }
+    
+    
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+       Node* temp= new Node(x);
+       if(head==NULL)
+       {
+           return temp;
+       }
+       Node* p = head;
+       while(p->next!=NULL)
+       {
+           p = p->next;
+       }
+       temp->next = NULL;
+       p->next = temp;
+       return head;
+    }
+};
