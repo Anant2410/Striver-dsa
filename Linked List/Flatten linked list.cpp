c0f10c2587579711473099ebe311fd8772024f1c
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-09-30 01:46:00 +0530

    Create Flatten linked list.cpp

---
+++ Linked List/Flatten linked list.cpp
@@ -0,0 +1,42 @@
+Node* f(Node* a, Node* b)
+{
+    Node* res = new Node(0);
+    Node* temp = res;
+    
+    while(a!=NULL && b!=NULL)
+    {
+        if(a->data <= b->data)
+        {
+            temp->bottom = a;
+            temp = temp->bottom;
+            a = a->bottom;
+        }
+        else
+        {
+           temp->bottom = b;
+            temp = temp->bottom;
+            b = b->bottom;  
+        }
+    }
+    if(a!=NULL)
+    {
+        temp->bottom = a;
+    }
+    else
+    {
+        temp->bottom = b;
+    }
+    return res->bottom;
+}
+Node *flatten(Node *root)
+{
+   if(root==NULL || root->next==NULL)
+   {
+       return root;
+   }
+   root->next = flatten(root->next);
+   
+   root = f(root,root->next);
+   
+   return root;
+}
