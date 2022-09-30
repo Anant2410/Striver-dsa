b53cda953a07ad9b6f119f1a00573576e63677d0
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-09-30 20:00:13 +0530

    Update Merge Without using Extra Space.cpp

--- Problems On Arrays/Hard/Merge Without using Extra Space.cpp
+++ Problems On Arrays/Hard/Merge Without using Extra Space.cpp
@@ -1,43 +1,35 @@
-int i, j, gap = n + m; 
-	
-	for (gap = nextGap(gap); gap > 0; gap = nextGap(gap)) 
-	{ 
-	    //Comparing elements in the first array itself with difference in 
-	    //index equal to the value of gap. 
-		for (i = 0; i + gap < n; i++) {
-		    //If element at ith index is greater than element at
-		    //(i+gap)th index, we swap them. 
-			if (arr1[i] > arr1[i + gap]){
-			    int temp = arr1[i];
-				arr1[i] = arr1[i + gap];
-				arr1[i + gap] = temp;
-			} 
-		}
-    
-		//Now comparing elements in both arrays with help of two pointers.
-		//The loop stops whenever any pointer exceeds the size of its array.
-		for (j = gap > n ? gap-n : 0 ; i < n&&j < m; i++, j++) {
-		    //If element in the first array is greater than element in
-		    //second array, we swap them. 
-			if (arr1[i] > arr2[j]){
-			    int temp = arr1[i];
-				arr1[i] = arr2[j];
-				arr2[j] = temp;
-			} 
-		}
+class Solution {
+public:
+    int f(int gap)
+    {
+        if(gap<=1) return 0;
         
-		if (j < m) 
-		{ 
-			//At last, comparing elements in the second array itself with 
-            //difference in index equal to the value of gap.
-			for (j = 0; j + gap < m; j++) {
-			    //If element at jth index is greater than element at
-		        //(j+gap)th index, we swap them. 
-				if (arr2[j] > arr2[j + gap]){
-				    int temp = arr2[j];
-				    arr2[j] = arr2[j + gap];
-				    arr2[j + gap] = temp;
-				} 
-			}
-		} 
-	} 
+        return (gap/2) + (gap%2);
+    }
+    void merge(vector<int>& nums1, int n, vector<int>& nums2, int m) {
+        int i,j,gap = n+m;
+        
+        for(gap = f(gap) ; gap > 0 ; gap = f(gap))
+        {
+            for(i=0;i+gap<n;i++)
+            {
+                if(nums1[i] > nums1[i+gap])
+                    swap(nums1[i], nums1[i+gap]);
+            }
+        }
+        
+        for(j = gap > n ? gap-n:0;i<n && j<m ; i++,j++)
+        {
+            if(nums1[i] > nums2[j])
+                swap(nums1[i],nums2[j]);
+        }
+        if(j<m)
+        {
+             for(j=0;j+gap<n;j++)
+            {
+                if(nums2[j] > nums2[j+gap])
+                    swap(nums2[j], nums2[j+gap]);
+            }
+        }
+    }
+};
