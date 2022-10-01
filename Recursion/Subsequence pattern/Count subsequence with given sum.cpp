b143dcc37ce13250fd93ecf644f9739fa28e786b
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-10-01 16:01:11 +0530

    Create Count subsequence with given sum.cpp

---
+++ Recursion/Subsequence pattern/Count subsequence with given sum.cpp
@@ -0,0 +1,24 @@
+/// recursion based solution so  gives tle the optimised approach in by using Dp;
+
+
+class Solution {
+public:
+    void f(int i, vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds)
+    {
+        if(i>=nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(nums[i]);
+        f(i+1,nums,ans,ds);
+        ds.pop_back();
+        f(i+1,nums,ans,ds);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        f(0,nums,ans,ds);
+        return ans;
+    }
+};
