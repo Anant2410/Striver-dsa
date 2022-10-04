9496fbc125c22b963bd908bf41410fbc6989d9cf
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-10-05 00:02:46 +0530

    Update rat in maze.cpp

--- Recursion/Subsequence pattern/rat in maze.cpp
+++ Recursion/Subsequence pattern/rat in maze.cpp
@@ -31,3 +31,30 @@ void f(int i, int j, vector<vector<int>> &m, int n, string path, vector<vector<i
         if(m[0][0] ==1) f(0,0,m,n,"",vis,ans);
         return ans;
     }
+//optimised one //
+
+void f(int i, int j, vector<vector<int>> &m, int n, string path, vector<vector<int>>& vis, 
+    vector<string>& ans, int di[] , int dj[]){
+        if(i==n-1 && j==n-1) {
+            ans.push_back(path);
+            return;
+        }
+        string dir = "DLRU";
+        for(int ind=0;ind<4;ind++){
+            int ni = i + di[ind];
+            int nj = j + dj[ind];
+            if(ni>=0 && nj>=0 && ni<n && nj<n && !vis[ni][nj] && m[ni][nj] ==1){
+                vis[i][j] = 1;
+                f(ni,nj,m,n,path + dir[ind],vis,ans,di,dj);
+                vis[i][j] = 0;
+            }
+        }
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        vector<vector<int>> vis(n, vector<int>(n,0));
+        int di[] = {1,0,0,-1};
+        int dj[] = {0,-1,1,0};
+        if(m[0][0] ==1) f(0,0,m,n,"",vis,ans,di,dj);
+        return ans;
+    }
