d81c926893ed6a5026869f0fe7f91fb011ef774a
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-10-06 19:10:13 +0530

    Update Count subsequence with given sum.cpp

--- Recursion/Subsequence pattern/Count subsequence with given sum.cpp
+++ Recursion/Subsequence pattern/Count subsequence with given sum.cpp
@@ -1,24 +1,23 @@
 /// recursion based solution so  gives tle the optimised approach in by using Dp;
 
 
-class Solution {
-public:
-    void f(int i, vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds)
-    {
-        if(i>=nums.size())
-        {
-            ans.push_back(ds);
-            return;
-        }
-        ds.push_back(nums[i]);
-        f(i+1,nums,ans,ds);
-        ds.pop_back();
-        f(i+1,nums,ans,ds);
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        f(0,nums,ans,ds);
-        return ans;
-    }
-};
+int f(int i, int arr[] , int n , int k, int sum)
+	{
+	    if(i==n)
+	    {
+	        if(sum==k)
+	        return 1;
+	        
+	        return 0;
+	    }
+	    sum += arr[i];
+	    int l = f(i+1,arr,n,k,sum);
+	    sum -= arr[i];
+	    int r = f(i+1,arr,n,k,sum);
+	    
+	    return l+r;
+	}
+	int perfectSum(int arr[], int n, int sum)
+	{
+        return f(0,arr,n,sum,0);
+	}
