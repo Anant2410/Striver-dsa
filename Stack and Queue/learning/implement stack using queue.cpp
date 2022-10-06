f6b064c39ca4ca7c2144acefab864f3e99e6f971
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-10-06 19:47:16 +0530

    Update implement stack using queue.cpp

--- Stack and Queue/learning/implement stack using queue.cpp
+++ Stack and Queue/learning/implement stack using queue.cpp
@@ -1,3 +1,4 @@
+// using single queue // 
 class MyStack {
 public:
     MyStack() {
@@ -39,3 +40,36 @@ public:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+
+
+
+
+
+
+
+
+
+////// gfg ///// (using 2 queue)
+void QueueStack :: push(int x)
+{
+        while(!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+}
+
+//Function to pop an element from stack using two queues. 
+int QueueStack :: pop()
+{
+        if(q1.empty()) return -1;
+        int x = q1.front();
+        q1.pop();
+        return x;
+}
