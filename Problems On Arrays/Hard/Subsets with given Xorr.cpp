388256ff5780fc4911252ceccd89bd1294e204cf
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-10-30 13:47:00 +0530

    Update Subsets with given Xorr.cpp

--- Problems On Arrays/Hard/Subsets with given Xorr.cpp
+++ Problems On Arrays/Hard/Subsets with given Xorr.cpp
@@ -15,3 +15,23 @@
         vector<vector<int>> dp(N+1, vector<int>(128,-1));
         return f(arr,N,K,dp);
     }
+
+
+//
+int Solution::solve(vector<int> &A, int B) {
+    unordered_map<int,int> mp;
+    int xorr=0,cnt=0;
+    
+    for(auto it: A){
+        xorr ^= it;
+        
+        if(xorr == B) cnt += 1;
+        
+        if(mp.find(xorr^B) != mp.end()){
+            cnt += mp[xorr^B];
+        }
+        mp[xorr] += 1;
+    }
+    return cnt;
+}
+
