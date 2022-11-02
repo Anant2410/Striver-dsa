06db8e9e32f5803989cea9473506686cb1faef4c
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-11-02 18:36:48 +0530

    Update Search in a matrix.cpp

--- Problems On Arrays/Easy/Search in a matrix.cpp
+++ Problems On Arrays/Easy/Search in a matrix.cpp
@@ -1,3 +1,5 @@
+// first row last column se traverse krnna hai agar bda hai to r++, agar chota to c--
+
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int r = 0, c = matrix[0].size()-1;
         int n = matrix.size();
