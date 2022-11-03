74e409b5f4605d742de0bf9d272b312ab8ac58c9
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-11-03 18:35:00 +0530

    Update kth largest element in array.cpp

--- Heap/medium/kth largest element in array.cpp
+++ Heap/medium/kth largest element in array.cpp
@@ -14,3 +14,27 @@ public:
         return minh.top();
     }
 };
+
+
+//gfg for k largest number(array return krni hai basically jo bhi heap me hai usse array me dalke return krdo)
+
+ vector<int> kLargest(int arr[], int n, int k)
+    {
+        vector<int> ans;
+        priority_queue<int,vector<int>,greater<int>>minh;
+        for(int i=0;i<n;i++)
+        {
+            minh.push(arr[i]);
+            if(minh.size() > k)
+            {
+                minh.pop();
+            }
+        }
+        while(!minh.empty())
+        {
+            ans.push_back(minh.top());
+            minh.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
