176ef1be2e034fa2fe49ba72033e29637e342b93
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-11-12 22:04:25 +0530

    Create insert intervals.cpp

---
+++ Greedy/Medium/insert intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& x) {
+       intervals.push_back(x);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        vector<int> temp = intervals[0];
+        
+        for(auto it: intervals)
+        {
+            if(it[0] <= temp[1])
+            {
+                temp[1] = max(temp[1], it[1]);
+            }
+            else
+            {
+                ans.push_back(temp);
+                temp = it;
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+    }
+};
