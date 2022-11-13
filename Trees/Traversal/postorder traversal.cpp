6633f7bdf91e57f06f20f75370fcf6cc6a60a7c6
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-11-13 19:34:43 +0530

    Update postorder traversal.cpp

--- Trees/Traversal/postorder traversal.cpp
+++ Trees/Traversal/postorder traversal.cpp
@@ -13,7 +13,35 @@ vector <int> postOrder(Node* root)
   return ans;
 }
 
-//iterative//
+// iterative using 2 stack//
+vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty())
+        {
+            TreeNode* node = st1.top();
+            st2.push(node);
+            st1.pop();
+            if(node->left)
+            {
+                st1.push(node->left);
+            }
+            if(node->right)
+            {
+                st1.push(node->right);
+            }
+        }
+        while(!st2.empty())
+        {
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
+    }
+
+//iterative using 1 stack//
  vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
         stack<TreeNode*>st;
