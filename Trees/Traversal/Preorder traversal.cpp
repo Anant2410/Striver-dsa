5450cfaec9f39a560155fbed1b92c41a760765a8
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-11-13 18:42:32 +0530

    Update Preorder traversal.cpp

--- Trees/Traversal/Preorder traversal.cpp
+++ Trees/Traversal/Preorder traversal.cpp
@@ -14,3 +14,35 @@ vector <int> preorder(Node* root)
     pre(root,ans);
     return ans;
 }
+
+// iterative//
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root, *prev;
+        if(root==NULL) return ans;
+        while(curr!= NULL){
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                prev = curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev = prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right= curr;
+                    ans.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
