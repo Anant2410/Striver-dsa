9705f139583b87836d21cd78e1c3118b1c089ba2
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-01-24 13:56:56 +0530

    Create graphs and vertices.cpp

---
+++ graphs/learning/graphs and vertices.cpp
@@ -0,0 +1,8 @@
+long long count(int n) {
+        int x = n*(n-1)/2;
+        long long ans = pow(2,x);
+        return ans;
+    }
+
+// number off grapgh wirth given vertices is
+// 2 ki ppower(n*n-1/2)
